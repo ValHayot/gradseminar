e6d50439d617eecd95daf210ba96ccea229dc3ca
valhayot <valeriehayot@gmail.com>
2020-11-24 18:00:35 -0500

    almost done sea chapter

---
+++ genfigs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+inkscape figures/nodes.svg -o figures/nodes.pdf
+inkscape figures/disks.svg -o figures/disks.pdf
+inkscape figures/threads.svg -o figures/threads.pdf
+inkscape figures/iterations.svg -o figures/iterations.pdf
