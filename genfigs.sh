1002e88a29ae01c0e64a7cdb25a7b790c9e2e81a
valhayot <valeriehayot@gmail.com>
2020-11-26 23:48:56 -0500

    final version

--- genfigs.sh
+++ genfigs.sh
@@ -4,3 +4,4 @@ inkscape figures/nodes.svg -o figures/nodes.pdf
 inkscape figures/disks.svg -o figures/disks.pdf
 inkscape figures/threads.svg -o figures/threads.pdf
 inkscape figures/iterations.svg -o figures/iterations.pdf
+inkscape figures/seadesign.svg -o figures/seadesign.pdf
